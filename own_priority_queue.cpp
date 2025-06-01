f67946359d2c6f518404e888ce04dc8e9fd0df77
Tomasz Wiśniewski <tomasz@wisniewski.app>
2025-06-01 11:10:26 +0200

    Add own priority queue impl

---
+++ own_priority_queue.cpp
@@ -0,0 +1,59 @@
+#include "own_priority_queue.h"
+
+void SimplePriorityQueue::sort_desc()
+{
+    std::sort(data.begin(), data.end(), [](const std::pair<int, int> &a, const std::pair<int, int> &b)
+              {
+                  if (a.first != b.first)
+                      return a.first > b.first; 
+                  return a.second > b.second; });
+}
+
+void SimplePriorityQueue::push(const std::pair<int, int> &value)
+{
+    data.push_back(value);
+    sort_desc();
+}
+
+void SimplePriorityQueue::pop()
+{
+    if (data.empty())
+        throw std::runtime_error("Queue is empty");
+    data.erase(data.begin());
+}
+
+const std::pair<int, int> &SimplePriorityQueue::top()
+{
+    if (data.empty())
+        throw std::runtime_error("Queue is empty");
+    return data[0];
+}
+
+const std::pair<int, int> &SimplePriorityQueue::second_top()
+{
+    if (data.size() < 2)
+        throw std::runtime_error("Less than 2 elements in the queue");
+    return data[1];
+}
+
+bool SimplePriorityQueue::x_is_in_first_k_elements(int x, int k)
+{
+    for (size_t i = 0; i < k; i++)
+    {
+        if (data[i].second == x)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool SimplePriorityQueue::empty() const
+{
+    return data.empty();
+}
+
+size_t SimplePriorityQueue::size() const
+{
+    return data.size();
+}
---
+++ own_priority_queue.h
@@ -0,0 +1,26 @@
+#ifndef SIMPLE_PRIORITY_QUEUE_H
+#define SIMPLE_PRIORITY_QUEUE_H
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <stdexcept>
+
+class SimplePriorityQueue
+{
+private:
+    std::vector<std::pair<int, int>> data;
+
+    void sort_desc();
+
+public:
+    void push(const std::pair<int, int> &value);
+    void pop();
+    const std::pair<int, int> &top();
+    const std::pair<int, int> &second_top();
+    bool x_is_in_first_k_elements(int x, int k);
+    bool empty() const;
+    size_t size() const;
+};
+
+#endif
